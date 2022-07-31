b8da315795bc5e9bd00f3cf0fed472da699fd449
Boris2001Tello <darckares2001@gmail.com>
2022-07-31 10:48:11 -0500

    primer comit

---
+++ Hilos/Proyecto2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a,b,mul;
+    printf("Digite primer numero:");
+    scanf( "%d", &a );
+    printf("Digite primer numero:");
+    scanf( "%d", &b );
+    mul = a+b;
+    printf( "La multiplicacion es: %d", mul );
+
+    return 0;
+
+}
---
+++ Planificador/Proyecto1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a,b,suma;
+    printf("Digite primer numero:");
+    scanf( "%d", &a );
+    printf("Digite primer numero:");
+    scanf( "%d", &b );
+    suma = a+b;
+    printf( "La suma es: %d", suma );
+
+    return 0;
+
+}
